6311b4b69535ca58ab2f1cb5ed36f4ef9bb2dd42
SovanIT <sovan@trisysit.com>
2018-12-22 20:47:25 +0530

    added test component.

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: ` <h2>
+                 Welcome {{name}}
+              </h2> `,
+  styles: [``]
+})
+export class TestComponent implements OnInit {
+   public name:string = 'Kushal';
+   
+  constructor() {} 
+
+  ngOnInit() {
+  }
+
+
+}
