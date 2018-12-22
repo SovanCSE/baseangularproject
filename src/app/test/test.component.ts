931d96b59ee674915c2b740b1f051bae865354fd
SovanCSE <sovan.panda338@gmail.com>
2018-12-22 23:59:03 +0530

    test

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -10,7 +10,9 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
    public name:string = 'Kushal';
    
-  constructor() {} 
+  constructor() {
+    
+  } 
 
   ngOnInit() {
   }
